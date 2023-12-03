0eada7d1ffc7e4f4e6b920c11c83d2d152b5c220
teyvi <teyviangela@yahoo.com>
2023-12-03 07:31:46 -0800

    debugging cod e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
-#!/bin/bash/python3
-
-for i in range(97, 123)
-if (i != 101 and i != 113):
-    print("{:c}".format(char), end='')
+#!/usr/bin/python3
+for i in range(97, 123):
+    if (i != 101 and i != 113):
+        print("{:c}".format(i), end='')
