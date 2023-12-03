7f697ecb9e90f8487b9fa4fab826beee6dc4d303
teyvi <teyviangela@yahoo.com>
2023-12-03 06:48:09 -0800

    if statement for the negatives

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,19 @@
 import random
 
 number = random.randint(-10000, 10000)
-reminder = number % 10
 
-if reminder > 5:
-    print(f"Last digit of {number} is {reminder} and is greater than 5")
-elif reminder == 0:
-    print(f"Last digit of {number} is {reminder} and is 0")
-elif 0 < reminder < 6:
-    print(f"Last digit of {number} is {reminder} and is less than 6 and not 0")
+if number >= 0:
+    last_digit = number % 10
+else:
+    last_digit = ((-number % 10) * -1)
+
+message = f"Last digit of {number} is {last_digit}"
+
+if last_digit == 0:
+    print(f"{message} and is 0")
+elif last_digit > 5 and last_digit % 10 != 0:
+    print(f"{message} and is greater than 5")
+elif 0 < last_digit < 6:
+    print(f"{message} and is less than 6 and not 0")
+else:
+    print(f"{message} and does not meet any specific condition")
