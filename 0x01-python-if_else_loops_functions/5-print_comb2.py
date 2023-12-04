d8136eb02db82aae93fe8f1ebc74e121bb293f42
teyvi <teyviangela@yahoo.com>
2023-12-03 16:11:28 -0800

    added variable -separator

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+# Use a separator variable to control the formatting
+separator = ""
 for num in range(0, 100):
-    print("{:02d}".format(num), end=', ')
+    print("{}{:02d}".format(separator, num), end='')
+    separator = ", "
+
+# Print a new line at the end
 print()
